1263f1dc05715c70898a689d6cfb407547ca8447
Bogdan Melnik <teh.ld86@gmail.com>
2017-11-18 23:24:36 +0300

    Parsing arguments

--- cli.cpp
+++ cli.cpp
@@ -1,9 +1,18 @@
 #include <cli.h>
 
+#include <utils.h>
+
 namespace NCli {
 	TArguments::TArguments(int argc, char **argv) {
+		const std::string prefix = "--";
+		const std::string sep = "=";
+
 		for (size_t i = 1; i < argc; ++i) {
-			std::cout << argv[i] << std::endl;
+			std::string argument{argv[i]};
+			if (NUtils::StartsWith(argument, prefix)) {
+				argument = argument.substr(prefix.length());
+				std::cout << argument << std::endl;
+			}
 		}
 	}
 }
